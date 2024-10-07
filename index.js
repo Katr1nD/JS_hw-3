37bd582f7d1bb9a3d4c3665555b88de9b1fa24b1
Kart1nD <kathrindrako@gmail.com>
2024-10-07 22:02:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Напишіть функцію delay(ms), яка повертає проміс, що виконується через ms мілісекунд.
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+// Виклик функції
+delay(2000).then(() => console.log('Пройшло 2 секунди'));
